e51859b7ca0ae7d0ee581ab1d4dd9e25e190319d
Simuratli <simuratli@gmail.com>
2025-01-15 14:08:44 +0400

    error

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -1,43 +1,46 @@
 import React from 'react'
-import {auth} from '@clerk/nextjs/server'
+import { auth } from '@clerk/nextjs/server'
 import { notFound, redirect } from 'next/navigation'
 import { db } from '@/lib/db'
 import BoardNavbar from '../../_components/board-navbar'
 
-const BoardLayout = async ({children,params}:{
-    children:React.ReactNode,
-    params:{boardId:string}
+const BoardLayout = async ({
+    children,
+    params
+}: {
+    children: React.ReactNode,
+    params: { boardId: string }
 }) => {
-    const boardId = (await params).boardId
-    const  {orgId} = await auth();
-    
+    const { boardId } = params;  // Remove the await here
+    const { orgId } = await auth();
 
-    if(!orgId) redirect("/select-org");
+    if (!orgId) {
+        redirect("/select-org");
+    }
 
     const board = await db.board.findUnique({
-        where:{
-            id:boardId,
+        where: {
+            id: boardId,
             orgId
         }
-    })
+    });
 
-    if(!board){
+    if (!board) {
         notFound();
     }
 
-
-  return (
-    <div 
-        className='relative h-full bg-no-repeat bg-cover bg-center'
-        style={{backgroundImage:`url(${board.imageFullUrl})`}}
-    >
-        <BoardNavbar data={board}/>
-        <div className='absolute inset-0 bg-black/10' />
-      <main className='relative pt-28 h-full'>
-        {children}
-      </main>
-    </div>
-  )
+    return (
+        <div 
+            className='relative h-full bg-no-repeat bg-cover bg-center'
+            style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+        >
+            <BoardNavbar data={board} />
+            <div className='absolute inset-0 bg-black/10' />
+            <main className='relative pt-28 h-full'>
+                {children}
+            </main>
+        </div>
+    )
 }
 
 export default BoardLayout
